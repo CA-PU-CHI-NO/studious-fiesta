3a39e393dd764bba05f02226896b37eeb30a4426
Polina Savitsckaya <polinasavitsckaja@gmail.com>
2021-03-15 16:28:08 +0300

    Проект без main

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Pract1
+{
+    class Program
+   /* {
+        static void Main(string[] args)
+        { 
+        }
+    }*/
+    {
+        private int nom;
+        private string data;
+        private float summ;
+
+        public Program(string data, int nom, float summ)
+        {
+            this.nom = nom;
+            this.data = data;
+            this.summ = summ;
+        }
+        public int Nom
+        {
+            get
+            {
+                return nom;
+            }
+            set
+            {
+                nom = value;
+            }
+        }
+        public string Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+        public float Summ
+        {
+            get
+            {
+                return summ;
+            }
+            set
+            {
+                summ = value;
+            }
+        }
+    }
+}
